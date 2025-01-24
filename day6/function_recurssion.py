a433a4bd13f14a4a4f9c7c2b2ce8c84acceb4b7a
DINESH MARDI <132223103+dineshmardi@users.noreply.github.com>
2025-01-24 15:55:48 +0000

    function practice

---
+++ day6/function_recurssion.py
@@ -0,0 +1,14 @@
+#function is blocks of code that can be used every time we want to use
+#redundant code is not used every time
+#tale input and gave some output...input are parameters
+
+def cal(a,b): #we defining how it will work
+    sum=a+b
+    return sum
+
+cal(3,4)
+cal(23.4,909)
+
+
+
+
